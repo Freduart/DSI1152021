7364c7532e4a4a7d0882a6ef891acfa5fa26852c
Francisco Ganuza <franganuza@hotmail.com>
2021-11-25 04:17:46 -0600

    consultas de graficos

--- app/Http/Controllers/EstadisticasController.php
+++ app/Http/Controllers/EstadisticasController.php
@@ -41,14 +41,28 @@ class EstadisticasController extends Controller
             }else if(Auth::user()->hasRole('Encargado Facultad')){
 
                 $idUsuario = Auth::id();
-                $encargado = EncargadoFacultad::where('user_id', '=', $idUsuario)->firstOrFail();
+                $encargado = EncargadoFacultad::where('user_id', '=', $idUsuario)->first();
                 $usuario = User::where('id', '=', $idUsuario);
-                $facultad = Facultad::where('id', '=', $encargado->facultad_id)->firstOrFail();
-                $carreras = Carrera::where('facultad_id', '=', $facultad->id)->firstOrFail();
+                $facultad = Facultad::where('id', '=', $encargado->facultad_id)->first();
                 // return($carreras);
-                $alumnosBySexo = DB::select(DB::raw("select count(sexo_estudiante) as cant from estudiantes where carrera_id in (:carreras) group by sexo_estudiante"),
-                                            array('carreras' => $carreras->id));
-                dd($alumnosBySexo);
+                $alumnosBySexo = DB::select(DB::raw("select count(sexo_estudiante) as cant from estudiantes 
+                    join carreras on carreras.id = estudiantes.carrera_id
+                    join facultades on facultades.id = carreras.facultad_id 
+                    where carreras.facultad_id = :facultadid group by sexo_estudiante"),
+                    array('facultadid' => $facultad->id));
+
+                $alumnosByEstado = DB::select(DB::raw("select count(estado_estudiante) as estado from estudiantes 
+                    join carreras on carreras.id = estudiantes.carrera_id
+                    join facultades on facultades.id = carreras.facultad_id 
+                    where carreras.facultad_id = :facultadid group by estado_estudiante"),
+                    array('facultadid' => $facultad->id));
+
+                $serviciosSocialesByEstado = DB::select(DB::raw("select count(estado_proyecto_social) as estado from peticiones s 
+                    join proyectos_sociales p on p.peticion_id = s.id
+                    join carreras c on c.id = s.carrera_id
+                    join facultades f on f.id = c.facultad_id 
+                    where c.facultad_id = :facultadid group by estado_proyecto_social"),
+                    array('facultadid' => $facultad->id));                     
 
             }else if(Auth::user()->hasRole('Administrador')){
                 $usuario = User::where('rol', '=', 'Administrador');
